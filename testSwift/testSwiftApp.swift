ab0e760b0a5c0f768eb70a3dfa1f80f180b73c58
EaBack <113344913+EaBack@users.noreply.github.com>
2023-03-03 11:07:57 +0100

    Initial Commit

---
+++ testSwift/testSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftApp.swift
+//  testSwift
+//
+//  Created by Edgar Backer on 2023-03-03.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
